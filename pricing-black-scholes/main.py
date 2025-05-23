e452b90478e959899ad34f93c0299743a335bfa3
Giuseppe di Sciacca | PA Imaging <giu.disciacca@gmail.com>
2025-05-23 08:23:45 +0200

    minor, commented placeholders

---
+++ pricing-black-scholes/main.py
@@ -0,0 +1,34 @@
+
+
+if __name__ == "__main__":
+    # Ticker	Nome
+    # run simulation of stock price with given volatilty and drift
+
+    # compute BS price of option
+
+    # Implement delta hedging and test that gains/losses are zero
+    
+    
+    tickers_collection = ['AAPL','MSFT','NVDA',	'AMD','TSLA','META','SPY']
+    for ticker_name in tickers_collection:
+        # download
+
+        # get options 
+
+        # estimate price with different volatilities
+
+        # estimate the actual volatility
+
+        # compare BS price w.r.t. real 
+
+        # payoff graph over time
+
+        # Implement delta hedging
+
+        # Compute losses/gains 
+
+        # Implement gamma, vega, rho
+
+        # compute losses/gains
+
+        
